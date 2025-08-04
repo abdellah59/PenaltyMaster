b8e033970e0894a7dac5124c961353749089f3d9
abdellah59 <abdellah.ait59@gmail.com>
2025-08-04 14:41:21 +0200

    feat: Add Game scene and script files for initial game setup

---
+++ Scripts/Game.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Game : Node2D
+{
+    public override void _Ready()
+    {
+        GD.Print("Jeu Lancé");
+    }
+
+    public override void _Process(double delta)
+    {
+        
+    }
+
+
+}
